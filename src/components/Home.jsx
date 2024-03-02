2f4aebf53d297d8ce03d2891581afbb701d46a49
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2024-03-02 19:07:06 +0500

    form: taking details from inputs fields and printing them on alert using useState

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from 'react';
 
-const Home = () => {
-  const [data, setData] = useState([]);
+function Home() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
 
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((response) => response.json())
-      .then((data) => setData(data));
-  }, []);
+  const handleSubmit = () => {
+    alert(`Name: ${name}\nAge: ${age}`);
+  };
 
   return (
     <div>
-      <h2>products</h2>
-      <ol>
-        {data.map((item) => (
-          <li key={item.id}>{item.title}</li>
-        ))}
-      </ol>
+      <h2>Enter your information:</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="age">Age:</label>
+          <input
+            type="number"
+            id="age"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          />
+        </div>
+        <button type="submit" className='bg-green-600 text-white rounded-lg px-2 my-3'>Submit</button>
+      </form>
     </div>
   );
-};
+}
 
 export default Home;
