87a6cdb4b00a8ff02a4305611eaab050fbc107fd
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2024-03-02 18:07:14 +0500

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,8 @@ const Home = () => {
     <div>
         <p>count : {count}</p>
         <div className='space-x-4'>
-        <button onClick={()=>setCount(count+1)} className='px-2 my-3 bg-blue-700 text-white '>Add</button>
-        
-        <button onClick={()=>setCount(count-1)} className='px-2 my-3 bg-yellow-500 text-white'>Minus</button>
+            <button onClick={()=>setCount(count+1)} className='px-2 my-3 bg-blue-700 text-white '>Add</button>
+            <button onClick={()=>setCount(count-1)} className='px-2 my-3 bg-yellow-500 text-white'>Minus</button>
         </div>
     </div>
   )
