443efbed7f757ef30b9c4351a11321127f8957d2
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2024-03-02 18:05:53 +0500

    implemented counter using useState hook

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React,{ useState } from 'react'
+
+const Home = () => {
+    const [count,setCount] = useState(0);
+  return (
+    <div>
+        <p>count : {count}</p>
+        <div className='space-x-4'>
+        <button onClick={()=>setCount(count+1)} className='px-2 my-3 bg-blue-700 text-white '>Add</button>
+        
+        <button onClick={()=>setCount(count-1)} className='px-2 my-3 bg-yellow-500 text-white'>Minus</button>
+        </div>
+    </div>
+  )
+}
+
+export default Home
